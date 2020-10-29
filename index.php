d38dfda6603b99ecc5afdee8ac234b5d3359511a
ShuuAkira <akagami112233@gmail.com>
2020-10-29 10:53:20 +0700

    base landing page

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XL Home Bandung</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-webfont.ttf">
+    <!-- END CSS -->
+
+    <!-- JS -->
+    <script src="assets/jQuery/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <!-- END JS -->
+</head>
+
+<body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal">XL HOME Bandung</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Produk</a>
+            <a class="p-2 text-dark" href="#">Kontak</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="#">Registrasi</a>
+    </div>
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">Paket Fiber XL</h1>
+        <p class="lead">Fiber Internet + TV Entertainment Box</p>
+    </div>
+
+    <div class="container">
+        <div class="card-deck mb-3 text-center">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">FAMILY</h3>
+                    <h2 class="card-subtitle"><img src="assets/img/family-speed.png" alt="" srcset=""></h2>
+                    <h2 class="card-subtitle mb-2"><sup>Rp</sup>349.000<small class="text-muted">/ bulan</small></h2>
+                    <p class="card-text">Baik digunakan untuk 1-10 komputer atau gadget.</p>
+                    <p class="card-text">Wi-Fi Fiber Modem</p>
+                    <p class="card-text">IP Dynamic Private</p>
+                    <a href="#" class="btn btn-outline-primary">Daftar Sekarang</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">SUPER USERS</h3>
+                    <h2 class="card-subtitle"><img src="assets/img/super-users-speed.png" alt="" srcset=""></h2>
+                    <h2 class="card-subtitle mb-2"><sup>Rp</sup>499.000<small class="text-muted">/ bulan</small></h2>
+                    <p class="card-text">Baik digunakan untuk 11-20 komputer atau gadget.</p>
+                    <p class="card-text">Wi-Fi Fiber Modem</p>
+                    <p class="card-text">IP Dynamic Private</p>
+                    <a href="#" class="btn btn-outline-primary">Daftar Sekarang</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">ULTIMATE</h3>
+                    <h2 class="card-subtitle"><img src="assets/img/ultimate-speed.png" alt="" srcset=""></h2>
+                    <h2 class="card-subtitle mb-2"><sup>Rp</sup>999.000<small class="text-muted">/ bulan</small></h2>
+                    <p class="card-text">Baik digunakan untuk 21-30 komputer atau gadget.</p>
+                    <p class="card-text">Wi-Fi Fiber Modem</p>
+                    <p class="card-text">IP Dynamic Private</p>
+                    <a href="#" class="btn btn-outline-primary">Daftar Sekarang</a>
+                </div>
+            </div>
+        </div>
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <small class="d-block mb-3 text-muted">&copy; 2020 - 2021 XLHOME Bandung. All Rights Reserved. <a href=""> Syarat & Ketentuan</a></small>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
