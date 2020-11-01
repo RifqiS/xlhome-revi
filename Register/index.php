438925c3820ae030433bd5a1eb0a13cc20abfb6c
ShuuAkira <akagami112233@gmail.com>
2020-11-01 15:27:57 +0700

    form registrasi, script requided & numberonly

--- Register/index.php
+++ Register/index.php
@@ -11,6 +11,10 @@
     <link rel="stylesheet" href="../assets/fonts/fontawesome-webfont.ttf">
     <!-- END CSS -->
 
+    <!-- ICON -->
+    <link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.min.css">
+    <!-- END ICON -->
+
     <!-- JS -->
     <script src="../assets/jQuery/jquery-3.3.1.min.js"></script>
     <script src="../assets/js/bootstrap.bundle.min.js"></script>
@@ -19,90 +23,106 @@
 
 <body>
     <?php
-        include "../nav.php";
+    include "../nav.php";
     ?>
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 class="display-4">Paket Fiber XL</h1>
         <p class="lead">Fiber Internet + TV Entertainment Box</p>
     </div>
 
+    <!-- Form Register -->
     <div class="container">
-        <div class="card-deck mb-3 text-center" style="align: center;">
-        <form action="">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <table>
-                        <tr>
-                            <td colspan="2">
-                            <div class="form-group">
-                                <label for="alamat">Alamat Lengkap</label>
-                                <input type="text" class="form-control" id="alamat" aria-describedby="alamatHelp" placeholder="Alamat Lengkap" size="200">
-                            </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                            <div class="form-group">
-                                <label for="nama">Nama Lengkap</label>
-                                <input type="text" class="form-control" id="nama" aria-describedby="namaHelp" placeholder="Alamat Lengkap" size="200">
-                            </div>
-                            </td>
-                            <td>
-                            <div class="form-group">
-                                <label for="email">E - Mail</label>
-                                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Alamat Lengkap" size="200">
-                            </div>
-                            </td>
-                        </tr>
-                        <tr>
-                        <td>
-                            <div class="form-group">
-                                <label for="noHp">No. Handphone</label>
-                                <input type="text" class="form-control" id="noHp" aria-describedby="noHpHelp" placeholder="Alamat Lengkap" size="200">
-                            </div>
-                            </td>
-                            <td>
-                            <div class="form-group">
-                                <label for="tlp">Telephone Rumah</label>
-                                <input type="text" class="form-control" id="tlp" aria-describedby="tlpHelp" placeholder="Alamat Lengkap" size="200">
-                            </div>
-                            </td>
-                        </tr>
-                        <tr>
-                        <td>
-                            <div class="form-group">
-                                <label for="upKTP">Upload Foto KTP</label>
-                                <input type="file" class="form-control" id="upKTP" aria-describedby="upKTPHelp">
-                            </div>
-                            </td>
-                            <td>
-                            <div class="form-group">
-                                <label for="upSelfie">Upload Foto Selfie Dengan KTP</label>
-                                <input type="file" class="form-control" id="upSelfie" aria-describedby="upSelfieHelp">
-                            </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </table>
+        <div class="row justify-content-md-center">
+            <form class="needs-validation" novalidate>
+
+                <div class="form-row">
+                    <div class="col-md mb-3">
+                        <label for="alamat">Alamat Lengkap</label>
+                        <textarea class="form-control btn-outline-primary" name="alamat" id="" cols="30" rows="3" placeholder="Alamat Lengkap" required></textarea>
+                        <div class="invalid-feedback">
+                            Tidak boleh kosong!
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </form>   
+
+                <div class="form-row">
+                    <div class="col-md-6 mb-3">
+                        <label for="nama">Nama Lengkap</label>
+                        <input type="text" class="form-control btn-outline-primary" name="nama" id="nama" placeholder="Nama Lengkap" required>
+                        <div class="invalid-feedback">
+                            Tidak boleh kosong!
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="email">E-mail</label>
+                        <input type="email" class="form-control btn-outline-primary" name="email" id="email" placeholder="alamat@contoh.com" required>
+                        <div class="invalid-feedback">
+                            Tidak boleh kosong!
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="col-md-6 mb-3">
+                        <label for="no_hp">No. Handphone</label>
+                        <input type="text" class="form-control btn-outline-primary" name="no_hp" id="no_hp" placeholder="08xxx" onkeypress="return isNumberKey(event)" required>
+                        <div class="invalid-feedback">
+                            Tidak boleh kosong!
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="no_telp">Telephone Rumah</label>
+                        <input type="text" class="form-control btn-outline-primary" name="no_telp" id="no_telp" placeholder="x2xxx" onkeypress="return isNumberKey(event)" required>
+                        <div class="invalid-feedback">
+                            Tidak boleh kosong!
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="col-md-6 mb-3">
+                        <label for="">Upload Foto KTP</label>
+                        <div class="form-group">
+                            <div class="custom-file">
+                                <input type="file" class="form-control custom-file-input" name="foto_ktp" id="foto_ktp" required>
+                                <label class="custom-file-label btn-outline-primary" for="foto_ktp">Piilh Foto</label>
+                            </div>
+                        </div>
+                        <div class="invalid-feedback">
+                            Tidak boleh kosong!
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="">Upload Foto Selfie Dengan KTP</label>
+                        <div class="custom-file">
+                            <input type="file" class="form-control custom-file-input" name="foto_wajah" id="foto_wajah" required>
+                            <label class="custom-file-label btn-outline-primary" for="foto_wajah">Piilh Foto</label>
+                        </div>
+                        <div class="invalid-feedback">
+                            Tidak boleh kosong!
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" name="alamat" id="setuju" required>
+                        <label class="form-check-label" for="setuju">
+                            Setuju dengan Syarat & Ketentuan.
+                        </label>
+                        <div class="invalid-feedback">
+                            Anda harus setuju dengan Syarat & Ketentuan terlebih dahulu.
+                        </div>
+                    </div>
+                </div>
+
+                <button class="btn btn-primary" name="kirim" type="submit">Kirim</button>
+
+            </form>
         </div>
+    </div>
+
+    <div class="container">
         <footer class="pt-4 my-md-5 pt-md-5 border-top">
             <div class="row">
                 <div class="col-12 col-md">
@@ -111,6 +131,38 @@
             </div>
         </footer>
     </div>
+
+    <!-- Number only -->
+    <script>
+        function isNumberKey(evt) {
+            var charCode = (evt.which) ? evt.which : event.keyCode
+            if (charCode > 31 && (charCode < 48 || charCode > 57))
+                return false;
+            return true;
+        }
+    </script>
+
+    <!-- Desain Form -->
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    </script>
 </body>
 
 </html>
