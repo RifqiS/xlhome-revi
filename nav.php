53bf404a4f6f605bbbc3513d6764fb07f7d96c94
ShuuAkira <akagami112233@gmail.com>
2020-11-01 16:28:40 +0700

    nav, base url

--- nav.php
+++ nav.php
@@ -1,8 +1,13 @@
+<?php
+// Base Url
+$base_url = 'http://localhost/2020/projek/xlhome-revi/';
+?>
+
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">XL HOME Bandung</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="#">Produk</a>
-            <a class="p-2 text-dark" href="#">Kontak</a>
-        </nav>
-        <a class="btn btn-outline-primary" href="Register/">Registrasi</a>
-    </div>
+    <h5 class="my-0 mr-md-auto font-weight-normal">XL HOME Bandung</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="<?= $base_url; ?>">Produk</a>
+        <a class="p-2 text-dark" href="<?= $base_url; ?>kontak">Kontak</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="<?= $base_url; ?>register">Registrasi</a>
+</div>
