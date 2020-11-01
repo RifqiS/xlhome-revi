7313b3ffc698d7793d85f27b39d039fb88370aec
ShuuAkira <akagami112233@gmail.com>
2020-11-01 22:39:11 +0700

    penambahan js number & validation

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>XL Home Bandung</title>
     <!-- Bikin Icon di Tab -->
-        <link rel="shortcut icon" href="assets/img/logo.png">
+    <link rel="shortcut icon" href="assets/img/logo.png">
     <!-- CSS -->
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/fonts/fontawesome-webfont.ttf">
@@ -20,76 +20,108 @@
 
 <body>
     <?php
-        include "pages/nav.php";
+    include "pages/nav.php";
     ?>
-            <div id="page-wrapper">
-	<div class="container" >
+    <div id="page-wrapper">
+        <div class="container">
 
-<?php 
+            <?php
 
-if (isset($_GET['page'])) {
+            if (isset($_GET['page'])) {
 
-	$page = $_GET['page'];
+                $page = $_GET['page'];
 
-	switch ($page) {
+                switch ($page) {
 
-		case 'Content':
-			include 'pages/content.php';
-        break;	
-        
-        case 'Settings':
-			include 'pages/settings.php';
-		break;
+                    case 'Content':
+                        include 'pages/content.php';
+                        break;
 
-        case 'Logout':
-			include 'pages/Logout.php';
-        break;
+                    case 'Settings':
+                        include 'pages/settings.php';
+                        break;
 
-        default:
-        ?>
-      		<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                <h1 class="display-4">Error 404</h1>
-                <p class="lead">Halaman Tidak Di Temukan Silahkan Kembali</p>
-            </div>
-		<?php
-        	break;
-	}
+                    case 'Logout':
+                        include 'pages/Logout.php';
+                        break;
 
-}else{
-    ?>
-    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 class="display-4">Paket Fiber XLHome</h1>
-        <p class="lead">Fiber Internet + TV Entertainment Box</p>
+                    default:
+            ?>
+                        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                            <h1 class="display-4">Error 404</h1>
+                            <p class="lead">Halaman Tidak Di Temukan Silahkan Kembali</p>
+                        </div>
+                <?php
+                        break;
+                }
+            } else {
+                ?>
+                <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                    <h1 class="display-4">Paket Fiber XLHome</h1>
+                    <p class="lead">Fiber Internet + TV Entertainment Box</p>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
     </div>
-<?php
-}
- ?>
-</div>
-</div>
-        <script>
-            $(document).ready(function(){
-                $('#tbl_poli').DataTable({
-                    responsive: true
-                });
+    <script>
+        $(document).ready(function() {
+            $('#tbl_poli').DataTable({
+                responsive: true
             });
-        </script>
+        });
+    </script>
 
-        <!-- jQuery -->
-        <script src="../assets/js/jquery.min.js"></script>
+    <!-- Number only -->
+    <script>
+        function isNumberKey(evt) {
+            var charCode = (evt.which) ? evt.which : event.keyCode
+            if (charCode > 31 && (charCode < 48 || charCode > 57))
+                return false;
+            return true;
+        }
+    </script>
 
-        <!-- Bootstrap Core JavaScript -->
-        <script src="../assets/js/bootstrap.min.js"></script>
+    <!-- Desain Form -->
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    </script>
 
-        <!-- Metis Menu Plugin JavaScript -->
-        <script src="../assets/js/metisMenu.min.js"></script>
+    <!-- jQuery -->
+    <script src="../assets/js/jquery.min.js"></script>
 
-        <!-- Morris Charts JavaScript -->
-        <script src="../assets/js/raphael.min.js"></script>
-        <script src="../assets/js/morris.min.js"></script>
-        <script src="../assets/js/morris-data.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../assets/js/bootstrap.min.js"></script>
 
-        <!-- Custom Theme JavaScript -->
-        <script src="../assets/js/startmin.js"></script>
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../assets/js/metisMenu.min.js"></script>
 
-    </body>
+    <!-- Morris Charts JavaScript -->
+    <script src="../assets/js/raphael.min.js"></script>
+    <script src="../assets/js/morris.min.js"></script>
+    <script src="../assets/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../assets/js/startmin.js"></script>
+
+</body>
+
 </html>
