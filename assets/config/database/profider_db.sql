5136eb8d25c84dc1b10ae948c80a9814bda0e5ea
RifqiS <mrifqisukmana9d@gmail.com>
2020-11-02 14:40:24 +0700

    update db

--- assets/config/database/profider_db.sql
+++ assets/config/database/profider_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Nov 2020 pada 05.15
+-- Waktu pembuatan: 02 Nov 2020 pada 08.39
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.7
 
@@ -39,13 +39,20 @@ CREATE TABLE `admin` (
   `agen1` text NOT NULL,
   `agen2` text NOT NULL,
   `tlp` int(15) NOT NULL,
-  `telp1` int(15) NOT NULL,
-  `telp2` int(15) NOT NULL,
+  `tlp1` int(15) NOT NULL,
+  `tlp2` int(15) NOT NULL,
   `wa` int(15) NOT NULL,
   `wa1` int(15) NOT NULL,
   `wa2` int(15) NOT NULL,
   `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id`, `user`, `pass`, `TitleHome`, `logo`, `sos1`, `sos2`, `agen`, `agen1`, `agen2`, `tlp`, `tlp1`, `tlp2`, `wa`, `wa1`, `wa2`, `alamat`) VALUES
+('Un!X1d@4pp', 'admin', 'admin', 'XLHome Bandung', '', '@XLHomeBandung', 'XLHomeBandung', 'Andi', 'Revi', 'Rini', 81224, 82335, 89644, 81224, 82335, 89644, 'Bandung Boi');
 
 -- --------------------------------------------------------
 
@@ -56,15 +63,23 @@ CREATE TABLE `admin` (
 CREATE TABLE `content` (
   `id` varchar(10) NOT NULL,
   `judul` text NOT NULL,
+  `logo` text NOT NULL,
+  `harga` int(10) NOT NULL,
   `sub_judul` text NOT NULL,
   `cont_1` text NOT NULL,
   `cont_2` text NOT NULL,
   `cont_3` text NOT NULL,
   `cont_4` text NOT NULL,
   `cont_5` text NOT NULL,
-  `cont_6` text NOT NULL,
-  `cont_7` text NOT NULL
+  `cont_6` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `content`
+--
+
+INSERT INTO `content` (`id`, `judul`, `logo`, `harga`, `sub_judul`, `cont_1`, `cont_2`, `cont_3`, `cont_4`, `cont_5`, `cont_6`) VALUES
+('1', '1', '', 1, '1', '1', '1', '1', '1', '1', '1');
 
 -- --------------------------------------------------------
 
