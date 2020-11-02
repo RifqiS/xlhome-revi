0196a3f3df97890243f3b9a90449238900571e58
RifqiS <mrifqisukmana9d@gmail.com>
2020-11-02 10:29:25 +0700

    update tambahan penampilan data

--- admin/pages/settings.php
+++ admin/pages/settings.php
@@ -14,7 +14,7 @@
                     <div class="form-group row">
                         <label for="id_aplikasi" class="col-sm-4 col-form-label">Id Aplikasi</label>
                         <div class="col-md">
-                            <input size="200" value="<?= $data['id'];?>" type="text" class="form-control border-primary" name="id_aplikasi" id="id_aplikasi" placeholder="" required>
+                            <input size="200" value="<?= $data["id"];?>" type="text" class="form-control border-primary" name="id_aplikasi" id="id_aplikasi" placeholder="" required>
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -23,7 +23,7 @@
                     <div class="form-group row">
                         <label for="username" class="col-sm-4 col-form-label">Username</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="username" id="username" placeholder="" required>
+                            <input size="200" value="<?= $data["user"];?>" type="text" class="form-control border-primary" name="username" id="username" placeholder="" required>
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -32,7 +32,7 @@
                     <div class="form-group row">
                         <label for="password" class="col-sm-4 col-form-label">Password</label>
                         <div class="col-md">
-                            <input size="200" type="password" class="form-control border-primary" name="password" id="password" placeholder="" required>
+                            <input size="200" value="<?= $data["pass"];?>" type="password" class="form-control border-primary" name="password" id="password" placeholder="" required>
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -41,7 +41,7 @@
                     <div class="form-group row">
                         <label for="title_home" class="col-sm-4 col-form-label">Title Home</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="title_home" id="title_home" placeholder="" required>
+                            <input size="200" value="<?= $data["TitleHome"];?>" type="text" class="form-control border-primary" name="title_home" id="title_home" placeholder="" required>
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -52,7 +52,7 @@
                         <div class="col-md">
                             <div class="form-group">
                                 <div class="custom-file">
-                                    <input size="200" type="file" class="form-control custom-file-input" name="logo" id="logo" required>
+                                    <input size="200" value="<?= $data["logo"];?>" type="file" class="form-control custom-file-input" name="logo" id="logo" required>
                                     <label class="custom-file-label border-primary" for="foto_ktp">Piilh Foto</label>
                                     <div class="invalid-feedback">
                                         Tidak boleh kosong!
@@ -64,7 +64,7 @@
                     <div class="form-group row">
                         <label for="alamat_kantor" class="col-sm-4 col-form-label">Alamat Kantor</label>
                         <div class="col-md">
-                            <textarea class="form-control border-primary" name="alamat_kantor" id="alamat_kantor" cols="30" rows="3" placeholder="Alamat Lengkap" required></textarea>
+                            <textarea class="form-control border-primary" name="alamat_kantor" id="alamat_kantor" cols="30" rows="3" placeholder="Alamat Lengkap" required><?= $data["alamat"];?></textarea>
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -78,7 +78,7 @@
                     <div class="form-group row">
                         <label for="media_sosial_1" class="col-sm-4 col-form-label">Sosial Media 1</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="media_sosial_1" id="media_sosial_1" placeholder="">
+                            <input size="200" value="<?= $data["sos1"];?>" type="text" class="form-control border-primary" name="media_sosial_1" id="media_sosial_1" placeholder="">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -87,7 +87,7 @@
                     <div class="form-group row">
                         <label for="media_sosial_2" class="col-sm-4 col-form-label">Sosial Media 2</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="media_sosial_2" id="media_sosial_2" placeholder="">
+                            <input size="200" value="<?= $data["sos2"];?>" type="text" class="form-control border-primary" name="media_sosial_2" id="media_sosial_2" placeholder="">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -97,7 +97,7 @@
                     <div class="form-group row">
                         <label for="nama_agen_1" class="col-sm-4 col-form-label">Nama</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="nama_agen_1" id="nama_agen_1" placeholder="">
+                            <input size="200" value="<?= $data["agen"];?>" type="text" class="form-control border-primary" name="nama_agen_1" id="nama_agen_1" placeholder="">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -106,7 +106,7 @@
                     <div class="form-group row">
                         <label for="no_tlp_agen_1" class="col-sm-4 col-form-label">No Telepon</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="no_tlp_agen_1" id="no_tlp_agen_1" placeholder="" onkeypress="return isNumberKey(event)">
+                            <input size="200" value="<?= $data["tlp"];?>" type="text" class="form-control border-primary" name="no_tlp_agen_1" id="no_tlp_agen_1" placeholder="" onkeypress="return isNumberKey(event)">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -115,7 +115,7 @@
                     <div class="form-group row">
                         <label for="no_wa_agen_1" class="col-sm-4 col-form-label">Whatsapp</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="no_wa_agen_1" id="no_wa_agen_1" placeholder="" onkeypress="return isNumberKey(event)">
+                            <input size="200" value="<?= $data["wa"];?>" type="text" class="form-control border-primary" name="no_wa_agen_1" id="no_wa_agen_1" placeholder="" onkeypress="return isNumberKey(event)">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -125,7 +125,7 @@
                     <div class="form-group row">
                         <label for="nama_agen_2" class="col-sm-4 col-form-label">Nama</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="nama_agen_2" id="nama_agen_2" placeholder="">
+                            <input size="200" value="<?= $data["agen1"];?>" type="text" class="form-control border-primary" name="nama_agen_2" id="nama_agen_2" placeholder="">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -134,7 +134,7 @@
                     <div class="form-group row">
                         <label for="no_tlp_agen_2" class="col-sm-4 col-form-label">No Telepon</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="no_tlp_agen_2" id="no_tlp_agen_2" placeholder="" onkeypress="return isNumberKey(event)">
+                            <input size="200" value="<?= $data["tlp1"];?>" type="text" class="form-control border-primary" name="no_tlp_agen_2" id="no_tlp_agen_2" placeholder="" onkeypress="return isNumberKey(event)">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -143,7 +143,7 @@
                     <div class="form-group row">
                         <label for="no_wa_agen_2" class="col-sm-4 col-form-label">Whatsapp</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="no_wa_agen_2" id="no_wa_agen_2" placeholder="" onkeypress="return isNumberKey(event)">
+                            <input size="200" value="<?= $data["wa1"];?>" type="text" class="form-control border-primary" name="no_wa_agen_2" id="no_wa_agen_2" placeholder="" onkeypress="return isNumberKey(event)">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -153,7 +153,7 @@
                     <div class="form-group row">
                         <label for="nama_agen_3" class="col-sm-4 col-form-label">Nama</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="nama_agen_3" id="nama_agen_3" placeholder="">
+                            <input size="200" value="<?= $data["agen2"];?>" type="text" class="form-control border-primary" name="nama_agen_3" id="nama_agen_3" placeholder="">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -162,7 +162,7 @@
                     <div class="form-group row">
                         <label for="no_tlp_agen_3" class="col-sm-4 col-form-label">No Telepon</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="no_tlp_agen_3" id="no_tlp_agen_3" placeholder="" onkeypress="return isNumberKey(event)">
+                            <input size="200" value="<?= $data["tlp2"];?>" type="text" class="form-control border-primary" name="no_tlp_agen_3" id="no_tlp_agen_3" placeholder="" onkeypress="return isNumberKey(event)">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
@@ -171,7 +171,7 @@
                     <div class="form-group row">
                         <label for="no_wa_agen_3" class="col-sm-4 col-form-label">Whatsapp</label>
                         <div class="col-md">
-                            <input size="200" type="text" class="form-control border-primary" name="no_wa_agen_3" id="no_wa_agen_3" placeholder="" onkeypress="return isNumberKey(event)">
+                            <input size="200" value="<?= $data["wa2"];?>" type="text" class="form-control border-primary" name="no_wa_agen_3" id="no_wa_agen_3" placeholder="" onkeypress="return isNumberKey(event)">
                             <div class="invalid-feedback">
                                 Tidak boleh kosong!
                             </div>
