20802c227c209e6ba231ea5830dfc39eff80f5c3
RifqiS <mrifqisukmana9d@gmail.com>
2020-11-02 10:28:52 +0700

    update script CRUD

--- assets/config/config.php
+++ assets/config/config.php
@@ -23,157 +23,86 @@ function query($query) {
        
 function iRegistrasi($iRegistrasi){
     global $db;
-    $id=$iRegistrasi['id'];
-    $alamat=htmlspecialchars($iRegistrasi['alamat']);
-    $paket=htmlspecialchars($iRegistrasi['paket']);
-    $nama=htmlspecialchars($iRegistrasi['nama']);
-    $email=htmlspecialchars($iRegistrasi['email']);
-    $noHp=htmlspecialchars($iRegistrasi['noHp']);
-    $tlp=htmlspecialchars($iRegistrasi['tlp']);
-    $fotoKtp=htmlspecialchars($iRegistrasi['fotoKtp']);
-    $fotoSelfie=htmlspecialchars($iRegistrasi['fotoSelfie']);
-    $query = "INSERT INTO spp VALUES('$id','$alamat','$paket','$nama','$email','$noHp','$tlp','$fotoKtp','$fotoSelfie','',0)";
+    $id         =$iRegistrasi['id'];
+    $alamat     =htmlspecialchars($iRegistrasi['alamat']);
+    $paket      =htmlspecialchars($iRegistrasi['paket']);
+    $nama       =htmlspecialchars($iRegistrasi['nama']);
+    $email      =htmlspecialchars($iRegistrasi['email']);
+    $noHp       =htmlspecialchars($iRegistrasi['noHp']);
+    $tlp        =htmlspecialchars($iRegistrasi['tlp']);
+    $fotoKtp    =htmlspecialchars($iRegistrasi['fotoKtp']);
+    $fotoSelfie =htmlspecialchars($iRegistrasi['fotoSelfie']);
+    $query      = "INSERT INTO registrasi VALUES('$id','$alamat','$paket','$nama','$email','$noHp','$tlp','$fotoKtp','$fotoSelfie','',0)";
     mysqli_query($db, $query);
     return mysqli_affected_rows($db);
 }
 
-function e_spp($e_spp){
+function iContent($iContent){
     global $db;
-    $id_spp=$e_spp['id_spp'];
-    $tahun=htmlspecialchars($e_spp['tahun']);
-    $nominal=htmlspecialchars($e_spp['nominal']);
-
-    $query="UPDATE spp SET tahun='$tahun',nominal='$nominal' WHERE id_spp='$id_spp'";
+    $id         =$iContent['id'];
+    $judul      =htmlspecialchars($iContent['judul']);
+    $logo       =htmlspecialchars($iContent['logo']);
+    $harga      =htmlspecialchars($iContent['harga']);
+    $subJudul   =htmlspecialchars($iContent['subJudul']);
+    $fitur1     =htmlspecialchars($iContent['fitur1']);
+    $fitur2     =htmlspecialchars($iContent['fitur2']);
+    $fitur3     =htmlspecialchars($iContent['fitur3']);
+    $fitur4     =htmlspecialchars($iContent['fitur4']);
+    $fitur5     =htmlspecialchars($iContent['fitur5']);
+    $fitur6     =htmlspecialchars($iContent['fitur6']);
+    $query      = "INSERT INTO content VALUES('$id','$judul','$logo','$harga','$subJudul','$fitur1','$fitur2','$fitur3','$fitur4','$fitur5','$fitur6',0)";
     mysqli_query($db, $query);
     return mysqli_affected_rows($db);
 }
 
-function h_spp($h_spp){
+function eAdmin($eAdmin){
     global $db;
-    $id_spp=$h_spp['id_spp'];
+    $id         =$eAdmin['id'];
+    $user       =htmlspecialchars($eAdmin['user']);
+    $pass       =htmlspecialchars($eAdmin['pass']);
+    $TitleHome  =htmlspecialchars($eAdmin['TitleHome']);
+    $logo       =htmlspecialchars($eAdmin['logo']);
+    $sos1       =htmlspecialchars($eAdmin['sos1']);
+    $sos2       =htmlspecialchars($eAdmin['sos2']);
+    $agen       =htmlspecialchars($eAdmin['agen']);
+    $agen1      =htmlspecialchars($eAdmin['agen1']);
+    $agen2      =htmlspecialchars($eAdmin['agen2']);
+    $tlp        =htmlspecialchars($eAdmin['tlp']);
+    $tlp1       =htmlspecialchars($eAdmin['tlp1']);
+    $tlp2       =htmlspecialchars($eAdmin['tlp2']);
+    $wa         =htmlspecialchars($eAdmin['wa']);
+    $wa1        =htmlspecialchars($eAdmin['wa1']);
+    $wa2        =htmlspecialchars($eAdmin['wa2']);
+    $alamat     =htmlspecialchars($eAdmin['alamat']);
 
-    $query="DELETE FROM spp WHERE id_spp = '$id_spp'";
+    $query      ="UPDATE 'admin' SET user='$user',pass='$pass',TitleHome='$TitleHome',logo='$logo',sos1='$sos1',sos2='$sos2',agen='$agen',agen1='$agen1',agen2='$agen2',tlp='$tlp',tlp1='$tlp1',tlp2='$tlp2',wa='$wa',wa1='$wa1',wa2='$wa2',alamat='$alamat' WHERE id='Un!X1d@4pp'";
     mysqli_query($db, $query);
     return mysqli_affected_rows($db);
 }
 
-function i_kelas($i_kelas){
+function eContent($eContent){
     global $db;
-    $id_kelas=$i_kelas['id_kelas'];
-    $nama_kelas=htmlspecialchars($i_kelas['nama_kelas']);
-    $kompetensi_keahlian=htmlspecialchars($i_kelas['kompetensi_keahlian']);
+    $id         =$eContent['id'];
+    $judul      =htmlspecialchars($eContent['judul']);
+    $logo       =htmlspecialchars($eContent['logo']);
+    $harga      =htmlspecialchars($eContent['harga']);
+    $subJudul   =htmlspecialchars($eContent['subJudul']);
+    $fitur1     =htmlspecialchars($eContent['fitur1']);
+    $fitur2     =htmlspecialchars($eContent['fitur2']);
+    $fitur3     =htmlspecialchars($eContent['fitur3']);
+    $fitur4     =htmlspecialchars($eContent['fitur4']);
+    $fitur5     =htmlspecialchars($eContent['fitur5']);
+    $fitur6     =htmlspecialchars($eContent['fitur6']);
 
-    $query = "INSERT INTO kelas VALUES('$id_kelas','$nama_kelas','$kompetensi_keahlian')";
+    $query      ="UPDATE content SET judul='$judul',logo='$logo',harga='$harga',subJudul='$subJudul',fitur1='$fitur1',fitur2='$fitur2',fitur3='$fitur3',fitur4='$fitur4',fitur5='$fitur5',fitur6='$fitur6' WHERE id='$id'";
     mysqli_query($db, $query);
     return mysqli_affected_rows($db);
 }
 
-function e_kelas($e_kelas){
+function hContent($hContent){
     global $db;
-    $id_kelas=$e_kelas['id_kelas'];
-    $nama_kelas=htmlspecialchars($e_kelas['nama_kelas']);
-    $kompetensi_keahlian=htmlspecialchars($e_kelas['kompetensi_keahlian']);
-
-    $query="UPDATE kelas SET nama_kelas='$nama_kelas',kompetensi_keahlian='$kompetensi_keahlian' WHERE id_kelas='$id_kelas'";
-    mysqli_query($db, $query);
-    return mysqli_affected_rows($db);
-}
-
-function h_kelas($h_kelas){
-    global $db;
-    $id_kelas=$h_kelas['id_kelas'];
-
-    $query="DELETE FROM kelas WHERE id_kelas = '$id_kelas'";
-    mysqli_query($db, $query);
-    return mysqli_affected_rows($db);
-}
-
-function i_petugas($i_petugas){
-    global $db;
-    $id_petugas=$i_petugas['id_petugas'];
-    $username=htmlspecialchars($i_petugas['username']);
-    $password=htmlspecialchars($i_petugas['password']);
-    $nama_petugas=htmlspecialchars($i_petugas['nama_petugas']);
-    $level=htmlspecialchars($i_petugas['level']);
-
-    $query = "INSERT INTO petugas VALUES('$id_petugas','$username','$password','$nama_petugas','$level')";
-    mysqli_query($db, $query);
-    return mysqli_affected_rows($db);
-}
-
-function e_petugas($e_petugas){
-    global $db;
-    $id_petugas=$e_petugas['id_petugas'];
-    $username=htmlspecialchars($e_petugas['username']);
-    $password=htmlspecialchars($e_petugas['password']);
-    $nama_petugas=htmlspecialchars($e_petugas['nama_petugas']);
-    $level=htmlspecialchars($e_petugas['level']);
-
-    $query="UPDATE petugas SET username='$username',password='$password',nama_petugas = '$nama_petugas', level = '$level' WHERE id_petugas='$id_petugas'";
-    mysqli_query($db, $query);
-    return mysqli_affected_rows($db);
-}
-
-function h_petugas($h_petugas){
-    global $db;
-    $id_petugas=$h_petugas['id_petugas'];
-
-    $query="DELETE FROM petugas WHERE id_petugas = '$id_petugas'";
-    mysqli_query($db, $query);
-    return mysqli_affected_rows($db);
-}
-
-function i_siswa($i_siswa){
-    global $db;
-    $nisn=$i_siswa['nisn'];
-    $nis=htmlspecialchars($i_siswa['nis']);
-    $nama=htmlspecialchars($i_siswa['nama']);
-    $id_kelas=htmlspecialchars($i_siswa['id_kelas']);
-    $alamat=htmlspecialchars($i_siswa['alamat']);
-    $no_telp=htmlspecialchars($i_siswa['no_telp']);
-    $id_spp=htmlspecialchars($i_siswa['id_spp']);
-    
-    $query = "INSERT INTO siswa VALUES('$nisn','$nis','$nama','$id_kelas','$alamat','$no_telp','$id_spp',0)";
-    mysqli_query($db, $query);
-    return mysqli_affected_rows($db);
-}
-
-function e_siswa($e_siswa){
-    global $db;
-    $nisn=$e_siswa['nisn'];
-    $nis=htmlspecialchars($e_siswa['nis']);
-    $nama=htmlspecialchars($e_siswa['nama']);
-    $id_kelas=htmlspecialchars($e_siswa['id_kelas']);
-    $alamat=htmlspecialchars($e_siswa['alamat']);
-    $no_telp=htmlspecialchars($e_siswa['no_telp']);
-    $id_spp=htmlspecialchars($e_siswa['id_spp']);
-
-    $query="UPDATE siswa SET nis='$nis',nama='$nama',id_kelas = '$id_kelas', alamat = '$alamat', no_telp = '$no_telp', id_spp = '$id_spp' WHERE nisn='$nisn'";
-    mysqli_query($db, $query);
-    return mysqli_affected_rows($db);
-}
-
-function h_siswa($h_siswa){
-    global $db;
-    $nisn=$h_siswa['nisn'];
-
-    $query="DELETE FROM siswa WHERE nisn = '$nisn'";
-    mysqli_query($db, $query); 
-    return mysqli_affected_rows($db);
-}
-
-function bayar_spp($bayar_spp){
-    global $db;
-    $id_pembayaran=$bayar_spp['id_pembayaran'];
-    $id_petugas=htmlspecialchars($bayar_spp['id_petugas']);
-    $nisn=htmlspecialchars($bayar_spp['nisn']);
-    $tgl_bayar=htmlspecialchars($bayar_spp['tgl_bayar']);
-    $bulan_bayar=htmlspecialchars($bayar_spp['bulan_bayar']);
-    $tahun_bayar=htmlspecialchars($bayar_spp['tahun_bayar']);
-    $id_spp=htmlspecialchars($bayar_spp['id_spp']);
-    $jumlah_bayar=htmlspecialchars($bayar_spp['jumlah_bayar']);
-
-    $query = "INSERT INTO pembayaran VALUES('$id_pembayaran','$id_petugas','$nisn','$tgl_bayar','$bulan_bayar','$tahun_bayar','$id_spp','$jumlah_bayar')";
+    $id=$hContent['id'];
+    $query="DELETE FROM content WHERE id = '$id'";
     mysqli_query($db, $query);
     return mysqli_affected_rows($db);
 }
