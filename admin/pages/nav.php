2a1decfc5120b0e2c05541189d40d0de7ab86c9d
ShuuAkira <akagami112233@gmail.com>
2020-11-21 16:08:36 +0700

    sub logo xlaxiata

--- admin/pages/nav.php
+++ admin/pages/nav.php
@@ -6,7 +6,9 @@
     }
 </style>
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 cnav text-white border-bottom shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal"><a href="<?= base_url(); ?>admin"><img src="<?= base_url(); ?>assets/img/app/<?= $app['logo']; ?>" style="height: 70px; weight: 100px;"> </h5>
+    <h5 class="my-0"><a href="<?= base_url(); ?>"><img src="<?= base_url(); ?>assets/img/app/<?= $app['logo']; ?>" style="height: 70px; weight: 100px;"> </h5>
+    <h5 class="my-0 cnav">With  &nbsp;</h5>
+    <h5 class="my-0 mr-md-auto"><a href="<?= base_url(); ?>"> <img src="<?= base_url(); ?>assets/img/app/<?= $app['logo2']; ?>" style="height: 40px; weight: 100px;"> </h5>
         <nav class="my-2 my-md-0 mr-md-3">
             <a class="p-2 text-white" href="?page=Registrasi">Registrasi</a>
             <a class="p-2 text-white" href="?page=Content">Content</a>
--- nav.php
+++ nav.php
@@ -8,12 +8,18 @@ $data = query("SELECT * FROM admin")[0];
         background-color: #002dbb;
         color: white;
     }
+
+    .tfon {
+        color: #002dbb;
+    }
 </style>
 
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 navbar-dark cnav text-white border-bottom shadow-sm">
-<h5 class="my-0 mr-md-auto font-weight-normal"><a href="<?= base_url(); ?>"><img src="<?= base_url(); ?>assets/img/app/<?= $data['logo']; ?>" style="height: 70px; weight: 100px;"> </h5>
-        <a class="p-2 cnav" href="<?= base_url(); ?>">Produk</a>
-        <a class="p-2 cnav" href="<?= base_url(); ?>kontak">Kontak</a>
+    <h5 class="my-0"><a href="<?= base_url(); ?>"><img src="<?= base_url(); ?>assets/img/app/<?= $data['logo']; ?>" style="height: 70px; weight: 100px;"> </h5>
+    <h5 class="my-0 cnav">With  &nbsp;</h5>
+    <h5 class="my-0 mr-md-auto"><a href="<?= base_url(); ?>"> <img src="<?= base_url(); ?>assets/img/app/<?= $data['logo2']; ?>" style="height: 40px; weight: 100px;"> </h5>
+    <a class="p-2 cnav" href="<?= base_url(); ?>">Produk</a>
+    <a class="p-2 cnav" href="<?= base_url(); ?>kontak">Kontak</a>
     </nav>
     <a class="btn btn-info" href="<?= base_url(); ?>register">Registrasi</a>
 </div>
